00bf3d2c578b4dbfa89b674240b4ad46318cba82
muhammadfajarps-dotcom <muhammadfajarps@gmail.com>
2026-01-26 05:39:29 +0700

    Update script.js
    Signed-off-by: muhammadfajarps-dotcom <muhammadfajarps@gmail.com>

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
 // ============================================================
 // 1. KONFIGURASI FIREBASE (TETAP SAMA)
 // ============================================================
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAyC3ZPW1XOciNwaJHOhkwSY8vFY1BRlz8",
-  authDomain: "mmrc-stock1999.firebaseapp.com",
-  databaseURL: "https://mmrc-stock1999-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "mmrc-stock1999",
-  storageBucket: "mmrc-stock1999.firebasestorage.app",
-  messagingSenderId: "486588564272",
-  appId: "1:486588564272:web:308b276a53401a738ebef5",
-  measurementId: "G-4218HRWRTC"
+  apiKey: "AIzaSyBdzWrKOBqrcu6talld7MN-2flHNibEWnE",
+  authDomain: "mmrc-stock.firebaseapp.com",
+  databaseURL: "https://mmrc-stock-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "mmrc-stock",
+  storageBucket: "mmrc-stock.firebasestorage.app",
+  messagingSenderId: "722563453659",
+  appId: "1:722563453659:web:b9f867367ecadb7a1df2fe"
 };
 
 // INITIALIZE FIREBASE
